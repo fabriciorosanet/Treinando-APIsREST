9b29470de76c24365168814b63f910c0fa9412b6
fabriciorosanet <161895481+fabriciorosanet@users.noreply.github.com>
2024-08-01 15:56:34 -0300

    API funcionando e o verbo HTTP Post funcionando com todas as validações

---
+++ Controllers/ValorantController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Valorant_APIsREST.Models;
+
+namespace Valorant_APIsREST.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ValorantController : Controller
+{
+   private List<Agente> agentes = new List<Agente>();
+
+
+    [HttpPost]
+   public void AdicionaAgente([FromBody] Agente agente)
+    {
+        agentes.Add(agente);
+        Console.WriteLine(agente.Nome);
+        Console.WriteLine(agente.Classe);
+        Console.WriteLine(agente.NumeroDoAgente);
+        Console.WriteLine(agente.QtdDeHorasJogadas);
+    }
+
+}
---
+++ Models/Agente.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Valorant_APIsREST.Models
+{
+    public class Agente
+    {
+        [Required(ErrorMessage = "O nome do agente é obrigatório")]
+        [StringLength(20, ErrorMessage = "Você excedeu o número de caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A classe do agente é obrigatória")]
+        [StringLength(15, ErrorMessage = "Você excedeu o número de caracteres")]
+        public string Classe { get; set; }
+        
+        [Required(ErrorMessage = "O número do agente é obrigatório")]
+        [Range(0, 10, ErrorMessage = "Número do agente invalido")]
+        public int NumeroDoAgente { get; set; }
+       
+        [Required(ErrorMessage = "A quantidade de horas jogadas é obrigatória")]
+        public double QtdDeHorasJogadas { get; set; }
+
+    }
+}
